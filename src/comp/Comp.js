0a792755077bc5867623dab2f08360ba99f51728
Your Name <you@example.com>
2025-08-05 17:57:23 +0900

    yarn

---
+++ src/comp/Comp.js
@@ -0,0 +1,17 @@
+import React,{ Component} from 'react';
+
+class Comp extends Component{
+    //리액트 초기에 사용하던 클래스형 컴포넌트
+    render(){
+        return(
+          <h1>[THIS IS IMPORTED COMPONENT]</h1>  
+        )
+    }
+}
+
+export default Component;
+/*
+컴포넌트는 특정 코드뭉치를 다른 부분에 이식하거나
+재사용하기 위해 사용하는 코드블록 단위를 말한다
+클래스형 컴포넌트
+*/
